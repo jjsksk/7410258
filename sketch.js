bec04034265932b2b3d3aada1b4734868b265d3e
jjsksk <shu19771112@gmail.com>
2025-05-23 11:34:46 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,10 +3,10 @@ let facemesh;
 let predictions = [];
 const indices = [409, 270, 269, 267, 0, 37, 39, 40, 185, 61, 146, 91, 181, 84, 17, 314, 405, 321, 375, 291];
 const indices2 = [76, 77, 90, 180, 85, 16, 315, 404, 320, 307, 306, 408, 304, 303, 302, 11, 72, 73, 74, 184];
-const indices3 = [243, 190, 56, 28, 27, 29, 30, 247, 130, 25, 110, 24, 23, 22, 26, 112];
-const indices4 = [133, 173, 157, 158, 159, 160, 161, 246, 33, 7, 163, 144, 145, 153, 154, 155];
-const indices5 = [359, 467, 260, 259, 257, 258, 286, 414, 463, 341, 256, 252, 253, 254, 339, 255];
-const indices6 = [263, 466, 388, 387, 386, 385, 384, 398, 362, 382, 381, 380, 374, 373, 390, 249];
+const indices3 = [133, 173, 157, 158, 159, 160, 161, 246, 33, 7, 163, 144, 145, 153, 154, 155];
+const indices4 = [243, 190, 56, 28, 27, 29, 30, 247, 130, 25, 110, 24, 23, 22, 26, 112];
+const indices5 = [263, 466, 388, 387, 386, 385, 384, 398, 362, 382, 381, 380, 374, 373, 390, 249];
+const indices6 = [359, 467, 260, 259, 257, 258, 286, 414, 463, 341, 256, 252, 253, 254, 339, 255];
 
 function setup() {
   createCanvas(640, 480).position(
@@ -33,14 +33,14 @@ function draw() {
   if (predictions.length > 0) {
     const keypoints = predictions[0].scaledMesh;
 
-    // 畫第一組紅色線
-    drawShape(indices, [255, 0, 0], 15, null);
+    // 畫第一組線條（#fde4cf，粗細15）
+    drawLine(indices, '#fde4cf', 15);
 
-    // 畫第二組紅色線並填滿黃色
-    drawShape(indices2, [255, 0, 0], 15, [255, 255, 0, 200]);
+    // 畫第二組線條並填滿（#fbf8cc）
+    drawShape(indices2, [255, 0, 0], 15, [251, 248, 204]);
 
-    // 在第一組與第二組之間充滿綠色
-    fill(0, 255, 0, 150); // 半透明綠色
+    // 在第一組與第二組之間充滿#ffcfd2
+    fill(255, 207, 210, 150); // 半透明#ffcfd2
     noStroke();
     beginShape();
     for (let i = 0; i < indices.length; i++) {
@@ -55,14 +55,14 @@ function draw() {
     }
     endShape(CLOSE);
 
-    // 畫第三組線條（#b9fbc0，粗細15）
-    drawLine(indices3, '#b9fbc0', 15);
+    // 畫第三組線條（#b9fbc0，粗細20）
+    drawLine(indices3, '#b9fbc0', 20);
 
     // 畫第四組線條並填滿（#b9fbc0）
     drawShape(indices4, [185, 251, 192], 15, [185, 251, 192]);
 
-    // 畫第五組線條（#b9fbc0，粗細15）
-    drawLine(indices5, '#b9fbc0', 15);
+    // 畫第五組線條（#b9fbc0，粗細20）
+    drawLine(indices5, '#b9fbc0', 20);
 
     // 畫第六組線條並填滿（#b9fbc0）
     drawShape(indices6, [185, 251, 192], 15, [185, 251, 192]);
